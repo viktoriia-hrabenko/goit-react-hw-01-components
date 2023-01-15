4efd0d088f606e0b0e05290e86d3b772940c778a
Viktoriia Hrabenko <vikagrabenko666@gmail.com>
2023-01-15 16:26:16 +0100

    FriendList

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from './Friend.module.css';
+import css from './FriendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => (
 
